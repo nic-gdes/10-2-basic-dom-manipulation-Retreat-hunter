e4e6dd5cac31d749d9c30849dc4ddc69157d9481
Retreat-hunter <hunterkorsen@gmail.com>
2023-11-02 13:56:23 -0700

    Done
    I really found it interesting how we did not mess with any of the HTML really, but rather messed with only Java

--- script.js
+++ script.js
@@ -3,23 +3,33 @@
 // 1. Link your javascript in the HTML above the closing </body> tag.
 
 // 2. Use querySelector to create a variable that contains the page's h1 title, and change the title using textContent into something a bit nicer.
-
+let title = document.querySelector("#page-title");
+title.textContent = "Hello I am a Big Dino Guy";
 // 3. Use querySelector to create a variable that contains the dinosaur image, and change the src to "./images/cute-dino.jpeg" so he doesn't look so mean. You can change an item's src by just calling the src method (item.src = "new_src";)
-
+const cuteDino = document.querySelector('#dino-img');
+cuteDino.src = './images/cute-dino.jpeg';
 // 4. Use querySelector to create a variable for the document body, and change the background color to something less harsh and intense.
-
+var background = document.body;
+background.style.backgroundColor = "lightgreen";
 // 5. Modify the following array to add at least 3 facts about the T. Rex that aren't so frightening. They don't need to be real facts.
 
-let dinoFacts = [
-    "Fact 1",
-    "Fact 2",
-    "Fact 3"
-]
+let dinoFacts = [ "Great Breed", "Big Arms", "Lovely Friends"];
+
+console.log(dinoFacts);
 
 // 6. Replace the facts on the page with our new array of facts:
 // 6.1 First, use querySelector to create a variable for the list.
-
+const list = document.querySelector('#facts')
 // 6.2 Then, remove all of the current content by setting your list variable's innerHTML to a blank string (variable.innerHTML = "")
+list.innerHTML = "";
 
 // 6.3 Then loop through the dinoFacts array, and appendChild each fact to the list variable. Remember you can loop through an array by using array.length to get the total number of items in the array to end the loop;
+for (let i = 0; i < dinoFacts.length; i++) {
+    const currentFacts = dinoFacts[i]; 
+    console.log(currentFacts);
+    const newListItem = document.createElement('li');
+    
+    newListItem.textContent = currentFacts;
 
+    list.appendChild(newListItem);
+}
